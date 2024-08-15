f7ac81d1854b440ce6c060b16170acb495d5fe7a
Roman Egoshin <egoshin.roman@gmail.com>
2024-08-15 20:14:56 +0200

    Added reset of task <ulRunTimeCounter> in vTaskGetInfo()

--- tasks.c
+++ tasks.c
@@ -6184,6 +6184,13 @@ static void prvCheckTasksWaitingTermination( void )
         #if ( configGENERATE_RUN_TIME_STATS == 1 )
         {
             pxTaskStatus->ulRunTimeCounter = pxTCB->ulRunTimeCounter;
+
+            #if ( configCLEAR_RUN_TIME_STATS == 1 )
+            {
+                pxTCB->ulRunTimeCounter = ( configRUN_TIME_COUNTER_TYPE ) 0;
+            }
+            #endif
+
         }
         #else
         {
